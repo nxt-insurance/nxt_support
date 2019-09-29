45297940b217a391dc5f8d85c6bd8771f32fb447
Nils Sommer <mail@nilssommer.de>
2019-09-29 18:08:14 +0200

    Replace minitest with rspec

--- test/nxt_support_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class NxtSupportTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::NxtSupport::VERSION
-  end
-end
--- test/test_helper.rb
+++
@@ -1,4 +0,0 @@
-$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
-require "nxt_support"
-
-require "minitest/autorun"
