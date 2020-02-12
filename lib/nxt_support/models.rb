33b423a317150df5a16eb1184c446580218cdea2
Nils Sommer <mail@nilssommer.de>
2020-02-12 14:57:52 +0100

    Add Email class with REGEXP

--- lib/nxt_support/models.rb
+++ lib/nxt_support/models.rb
@@ -1,1 +1,2 @@
+require "nxt_support/models/email"
 require "nxt_support/models/safely_find_or_createable"
---
+++ spec/models/email_spec.rb
@@ -0,0 +1,25 @@
+RSpec.describe NxtSupport::Email do
+  describe '::REGEXP' do
+    subject { NxtSupport::Email::REGEXP }
+
+    let(:valid_emails) do
+      %w[john.doe@example.org john@example.co.uk john@ex-ample.org]
+    end
+
+    let(:invalid_emails) do
+      ['john doe@example.org', 'john@.example.org', 'john@example']
+    end
+
+    it 'matches valid emails' do
+      valid_emails.each do |valid_email|
+        expect(subject).to match valid_email
+      end
+    end
+
+    it 'does not match invalid emails' do
+      invalid_emails.each do |invalid_email|
+        expect(subject).not_to match invalid_email
+      end
+    end
+  end
+end
