bea223360a7b6dc5049755b639758119a50260fa
dud <v.dudnyk@i.ua>
2020-06-16 22:15:58 +0300

    Adjust test cases

--- spec/util/hash_translator_spec.rb
+++ spec/util/hash_translator_spec.rb
@@ -15,15 +15,15 @@ RSpec.describe NxtSupport::HashTranslator do
     }
   end
 
-  let(:tuples) do
-    {
-      firstname: :first_name,
-      phonenumber: { phone_number: ->(phone){ 'Mobile: ' + phone.to_s } },
-      havenoideakey: %i[why_anyone needs_same_value but_array_of_keys? will_ask]
-    }
-  end
-
   context 'happy path' do
+    let(:tuples) do
+      {
+        firstname: :first_name,
+        phonenumber: { phone_number: ->(phone){ 'Mobile: ' + phone.to_s } },
+        havenoideakey: %i[why_anyone needs_same_value but_array_of_keys? will_ask]
+      }
+    end
+
     it do
       expect(subject).to eq(
         'first_name' => 'Lebowsky',
@@ -33,6 +33,40 @@ RSpec.describe NxtSupport::HashTranslator do
         'but_array_of_keys?' => 'Ideas?',
         'will_ask' => 'Ideas?'
       )
+    end
+  end
+
+  context 'with invalid tuple arguments' do
+    context 'as a hash' do
+      context 'with multiple key-value pairs' do
+        let(:tuples) do
+          {
+            firstname: :first_name,
+            phonenumber: { phone_number: ->(phone){ 'Mobile: ' + phone.to_s }, unused_key: :unused_value },
+            havenoideakey: %i[why_anyone needs_same_value but_array_of_keys? will_ask]
+          }
+        end
+
+        it 'raise an error' do
+          expect { subject }.to raise_exception(NxtSupport::HashTranslator::HashTranslationService::InvalidTranslationArgument)
+            .with_message(/must contain only 1 key-value pair!/)
+        end
+      end
+
+      context 'with uncallable block' do
+        let(:tuples) do
+          {
+            firstname: :first_name,
+            phonenumber: { phone_number: :uncallable_block },
+            havenoideakey: %i[why_anyone needs_same_value but_array_of_keys? will_ask]
+          }
+        end
+
+        it 'raise an error' do
+          expect { subject }.to raise_exception(NxtSupport::HashTranslator::HashTranslationService::InvalidTranslationArgument)
+            .with_message('uncallable_block must be a callable block!')
+        end
+      end
     end
   end
 end
