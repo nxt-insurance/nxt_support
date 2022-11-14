525de2209346390d7801c53891e74536e2307770
Shalvah Adebayo <104110245+shalvah-gs@users.noreply.github.com>
2022-11-14 16:10:23 +0100

    Clean up condition
    Co-authored-by: Vlad Rudnev <93906214+vrudnev@users.noreply.github.com>

--- lib/nxt_support/middleware/sentry_error_id.rb
+++ lib/nxt_support/middleware/sentry_error_id.rb
@@ -7,8 +7,8 @@ module NxtSupport
 
       def call(env)
         status, headers, body = @app.call(env)
-        if status >= 500
-          headers["Sentry-Error-ID"] = env['sentry.error_event_id'] if env['sentry.error_event_id']
+        if status >= 500 && env['sentry.error_event_id']
+          headers["Sentry-Error-ID"] = env['sentry.error_event_id']
         end
         [status, headers, body]
       end
