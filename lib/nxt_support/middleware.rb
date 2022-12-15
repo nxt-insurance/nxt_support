07c59aa89ac9e576e93ec38295229e9e5616ced7
Shalvah Adebayo <104110245+shalvah-gs@users.noreply.github.com>
2022-12-15 18:32:53 +0100

    `Sentry-Error-Id`: Set `Access-Control-Expose-Headers` so frontend can access header (#105)

--- lib/nxt_support/middleware.rb
+++
@@ -1,1 +0,0 @@
-require_relative 'middleware/sentry_error_id'
--- lib/nxt_support/middleware/sentry_error_id.rb
+++ lib/nxt_support/middleware/sentry_error_id.rb
@@ -8,7 +8,9 @@ module NxtSupport
       def call(env)
         status, headers, body = @app.call(env)
         if status >= 500 && env['sentry.error_event_id']
-          headers["Sentry-Error-Id"] = env['sentry.error_event_id']
+          headers['Sentry-Error-Id'] = env['sentry.error_event_id']
+          exposed_headers = headers['Access-Control-Expose-Headers'] || headers['access-control-expose-headers']
+          headers['Access-Control-Expose-Headers'] = [exposed_headers, 'Sentry-Error-Id'].compact.join(', ')
         end
         [status, headers, body]
       end
